971611f1435fb623317a41aabae3ba0e2c4f7f60
Pramod Sendanayake <pcsendanayake@virtusapolaris.com>
2017-01-15 21:17:40 +0530

    added keyword driven changes

---
+++ src/com/selenium/pages/HomePage.java
@@ -0,0 +1,12 @@
+package com.selenium.pages;
+
+import org.openqa.selenium.By;
+
+public class HomePage {
+
+
+	public static By tf_userName = By.xpath("//input[@name='userName']");
+	public static By tf_password = By.xpath("//input[@name='password']");
+	public static By btn_signIn = By.xpath("//input[@name='login']");
+	
+}
